a7225bc71c80bc55dadc4daffb1a4bdc5f3187e3
nativel <nativelbk@gmail.com>
2025-05-23 21:42:27 +0300

    feat: LinearSearch implemented

---
+++ List/Search/LinearSearch.ts
@@ -0,0 +1,8 @@
+export function linearSearch(array: number[], needle: number): boolean {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === needle) {
+      return true;
+    }
+  }
+  return false;
+}
