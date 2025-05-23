1849d331e10ef6f7d76a6db8877c30a50c88a1be
nativel <nativelbk@gmail.com>
2025-05-23 21:50:00 +0300

    feat: added recursive version for linear search

---
+++ List/Search/LinearSearchRecursive.ts
@@ -0,0 +1,21 @@
+function recurse(array: number[], needle: number, i: number): boolean {
+  //base
+  if (i === array.length) {
+    return false;
+  }
+
+  //pre
+  if (array[i] === needle) {
+    return true;
+  }
+
+  //recurse
+  return recurse(array, needle, i + 1);
+}
+
+export default function linearSearchRecursive(
+  array: number[],
+  needle: number
+): boolean {
+  return recurse(array, needle, 0);
+}
