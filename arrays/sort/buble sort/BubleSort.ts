60e7fa68b75bd34049c70da9dcba2ceae15d9f93
nativel <nativelbk@gmail.com>
2025-05-26 18:44:01 +0300

    feat: added buble sort algorithm

---
+++ arrays/sort/buble sort/BubleSort.ts
@@ -0,0 +1,13 @@
+//O(n^2)
+
+export function BubleSort(array: number[]): void {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        const tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+}
