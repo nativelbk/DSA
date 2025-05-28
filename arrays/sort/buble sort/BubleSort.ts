3225496c26d610010c53cc14a92da1bbd91971f0
nativel <nativelbk@gmail.com>
2025-05-28 13:34:19 +0300

    fix: change signature

--- arrays/sort/buble sort/BubleSort.ts
+++ arrays/sort/buble sort/BubleSort.ts
@@ -1,8 +1,6 @@
-import { ArrayT } from "../../../types/types";
-
 //O(n^2)
 
-export function BubleSort({ array }: ArrayT<number>): void {
+export function BubleSort<T>(array: T[]): void {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1 - i; j++) {
       if (array[j] > array[j + 1]) {
