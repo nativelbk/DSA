6ebdade79155ab97643bfa52a4f361a9538fa882
nativel <nativelbk@gmail.com>
2025-05-28 10:58:34 +0300

    feat: updated type

--- type/type.ts
+++ type/type.ts
@@ -3,7 +3,17 @@ export type SinglyLinkedList<T> = {
   next?: SinglyLinkedList<T>;
 };
 
+export type DoublyLinkedList<T> = {
+  value: T;
+  next?: DoublyLinkedList<T>;
+  previous?: DoublyLinkedList<T>;
+};
+
 export type SearchInArray<T> = {
   array: T[];
   needle: T;
+};
+
+export type ArrayT<T> = {
+  array: T[];
 };
